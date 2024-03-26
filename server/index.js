15f9e3e0453b9a2f0b2237b467e2773aec036446
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-03-25 22:29:41 -0300

    basic setup

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ const app = express()
 
 const db = require('./models')
 
+app.use(express.json())
+
+// Routers
+const userRouter = require('./routes/Users')
+app.use("/users", userRouter)
+
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {
         console.log("Server running on port 3001")
---
+++ server/routes/Users.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const { Users } = require("../models")
+
+router.get("/", async (req, res) => {
+    const listOfUsers = await Users.findAll()
+    res.json(listOfUsers)
+})
+
+router.post("/", async (req, res) => {
+    const user = req.body
+    await Users.create(user)
+    res.json(user)
+})
+
+module.exports = router
