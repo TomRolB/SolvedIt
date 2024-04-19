09c7a4c83de9df802b7c8003476b89cd4d2b70f3
FranCalveyra <121242677+FranCalveyra@users.noreply.github.com>
2024-04-19 19:22:50 -0300

    InviteLink working (#27)
    * Link Enter view
    
    * Enrolling to other people's classes works, need to check if they were already enrolled (may need to create another endpoint in class router)
    
    * Added endpoint to check if student is already enrolled
    
    * Enrolling by link working correctly

--- server/routes/Class.js
+++ server/routes/Class.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { Class, IsInClass, Users} = require("../models")
+const { Class, IsInClass, Users, InviteLink} = require("../models")
 const bodyParser = require("body-parser")
 const Auth = require("../controllers/Auth");
 router.use(bodyParser.urlencoded({extended: true}))
 const db = require("../models/index")
+const {Sequelize} = require("sequelize");
 router.post("/create-class", async (req, res) => {
     const classInfo = req.body
     // console.log(classInfo.uuid)
@@ -13,6 +14,7 @@ router.post("/create-class", async (req, res) => {
     const maxId = await Class.max('id');
     await console.log(maxId)
     await IsInClass.create({userId: userId, classId: maxId})
+    InviteLink.create({classId: maxId, link: `http://www.solvedit.com/enroll-to/${maxId}`,userCount:0})
     // res.json(classInfo)
 })
 
@@ -34,6 +36,22 @@ router.put("/byId/:id/edit", async (req, res) => {
     console.log(id)
     await Class.update({name: classInfo.name, description: classInfo.description}, {where: {id: id}})
     res.json({message: "Class updated"})
+})
+
+router.post("/:uuid/enroll-to/:id", async(req,res) =>{
+    const classId = req.params.id
+    const userId = Auth.getUserId(req.params.uuid).id
+    const [entry, created] = await IsInClass.findOrCreate({where:{userId: userId, classId: Number(classId)}});
+    if(!created)
+    InviteLink.update({userCount: Sequelize.literal('userCount + 1')}, {where: {classId: classId}});
+    res.json({message: "Successfully enrolled"})
+})
+
+router.get("/:uuid/enrolled-in/:id", async(req,res) =>{
+    const classId = req.params.id
+    const userId = Auth.getUserId(req.params.uuid).id
+    const isInClass = await IsInClass.findAll({where:{classId: Number(classId), userId: userId}})
+    res.send(isInClass)
 })
 
 module.exports = router
