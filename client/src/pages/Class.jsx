c81de5dd3501e403391659793ebe38b335fbe47f
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-05-14 00:46:53 -0300

    pages for tags

--- client/src/pages/Class.jsx
+++ client/src/pages/Class.jsx
@@ -53,13 +53,23 @@ export function Class({uuid, setUuid, classId, setClassId}) {
             <Navbar></Navbar>
             <div className="h-screen bg-gradient-to-tr from-white to-blue-300 p-5">
                 <CourseInfo/>
-                {isAdmin ? <button type="button"
+                {isAdmin ?
+                    <button type="button"
                                    className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-                    <a href={"/class/" + id + "/invites"}><i className="fa-solid fa-plus"></i> Manage Invitations</a>
-                </button> : <button type="button"
-                                    className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-                    <a href={"/class/" + id + "/post-question"}><i className="fa-solid fa-plus"></i> Post Question</a>
-                </button>}
+                        <a href={"/class/" + id + "/invites"}><i className="fa-solid fa-plus"></i> Manage Invitations</a>
+                    </button>
+                    :
+                    <div>
+                        <button type="button"
+                                className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                            <a href={"/class/" + id + "/post-question"}><i className="fa-solid fa-plus"></i> Post Question</a>
+                        </button>
+                        <button type="button"
+                                className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                            <a href={"/class/" + id + "/create-tag"}><i className="fa-solid fa-plus"></i> Create Tag</a>
+                        </button>
+                    </div>
+                }
                 {/*<Questions/>*/}
                 {questions.length > 0 ? questions : <h1>{"There are no questions yet"}</h1>}
             </div>
--- client/src/pages/PostQuestion.jsx
+++ client/src/pages/PostQuestion.jsx
@@ -1,10 +1,13 @@
 import {useState} from "react";
 import axios from "axios";
 import {useParams} from "react-router-dom";
+import {Navbar} from "../components/Navbar";
+import "../styles/Tag.css"
 
 export function PostQuestion() {
     const [title, setTitle] = useState("")
     const [description, setDescription] = useState("")
+    const [tags, setTags] = useState([])
     let {id} = useParams()
 
     function handleTitleChange(event) {
@@ -28,11 +31,29 @@ export function PostQuestion() {
             .catch((err) => console.log(err))
     }
 
-    return <form onSubmit={handleFormSubmit}>
-        <label>Title</label><br/>
-        <input onChange={handleTitleChange} type={"text"}/><br/>
-        <label>Description</label><br/>
-        <input onChange={handleDescriptionChange} type={"text"}/><br/>
-        <input type={"submit"}/>
-    </form>
+    return (
+        <div>
+            <Navbar></Navbar>
+            <form onSubmit={handleFormSubmit}>
+                <label>Title</label><br/>
+                <input onChange={handleTitleChange} type={"text"}/><br/>
+                <label>Description</label><br/>
+                <input onChange={handleDescriptionChange} type={"text"}/><br/>
+                <label>Tag</label><br/>
+                <div className="card">
+                    <span className="title">All tags</span>
+                    <div className="card__tags">
+                        <ul className="tag">
+                            <li className="tag__name">JS</li>
+                            <li className="tag__name">wordpress</li>
+                            <li className="tag__name">uiverse</li>
+                            <li className="tag__name">Css</li>
+                            <li className="tag__name">html</li>
+                        </ul>
+                    </div>
+                </div>
+                <input type={"submit"}/>
+            </form>
+        </div>
+    )
 }
