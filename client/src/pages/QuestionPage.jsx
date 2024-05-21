7c04b647dab30dab39ddc8147146eebc4b2d7024
Tomás <troldanborjas@mail.austral.edu.ar>
2024-05-21 12:23:37 -0300

    Minor upvote style changes

--- client/src/pages/QuestionPage.jsx
+++ client/src/pages/QuestionPage.jsx
@@ -295,7 +295,7 @@ export function QuestionPage() {
                         className={
                             hasUserVotedIt
                                 ? "text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800"
-                                : "text-black bg-white-700 hover:bg-white-800 focus:ring-4 focus:ring-white-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-white-600 dark:hover:bg-white-700 focus:outline-none dark:focus:ring-white-800"
+                                : "text-white bg-gray-700 px-5 py-2.5 me-2 mb-2 cursor-default"
                 }>
                     <i className="fa-solid fa-arrow-up"></i>
                     {" " + voteCount}
