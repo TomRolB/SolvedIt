4082da506cbf40db30809482eb7ed10ba10dee1e
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-05-24 22:49:42 -0300

    filtering without questions no longer returns error

--- server/routes/Filter.js
+++ server/routes/Filter.js
@@ -4,8 +4,11 @@ const Auth = require('../controllers/Auth')
 const {IsInClass, Votes} = require('../models/')
 const Tags = require("../controllers/Tags");
 const Filter = require("../controllers/Filter");
+const Question = require("../controllers/Questions");
 router.get('/filter-by-tags', async (req, res) => {
     const classId = req.query.classId
+    const questions = await Question.getQuestionsOfClass(classId)
+    if (!Array.isArray(questions) || !questions.length) return
     const result = await Filter.getSelectedQuestionsOfClass(classId, req.query.tags, req.query.showDeleted)
     res.json(result)
 })
