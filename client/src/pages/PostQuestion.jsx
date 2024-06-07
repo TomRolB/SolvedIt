92ee5c3e82b3b2ece234c1679a4a90b9d23decf8
Tomás <troldanborjas@mail.austral.edu.ar>
2024-06-07 16:33:40 -0300

    Names of selected files are now displayed

--- client/src/pages/PostQuestion.jsx
+++ client/src/pages/PostQuestion.jsx
@@ -80,7 +80,8 @@ export function PostQuestion() {
                             <input onChange={handleTitleChange} type={"text"} className={"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"}/><br/>
                             <label className="block text-lg font-bold dark:text-white mb-2">Description</label>
                             <input className={"block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"} onChange={handleDescriptionChange} type={"text"}/><br/>
-                            <input type="file" onChange={handleFileChange}/><br/>
+                            <input type="file" onChange={handleFileChange} className="text-transparent"/><br/>
+                            {files.map(file => <p className="text-white">{file.name}</p>)}
                             <Select
                                 closeMenuOnSelect={false}
                                 isMulti
