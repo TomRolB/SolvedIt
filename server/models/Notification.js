bf0f35ab1db281007ec74159549ae823cc1c4747
Francisco Calveyra <121242677+FranCalveyra@users.noreply.github.com>
2024-06-08 14:32:27 -0300

    Revert "Revert "11_notifications (#59)" (#60)" (#61)
    This reverts commit 052992912d804e88dc72009b806a3e7ea6d88d0d.

---
+++ server/models/Notification.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define("Notification", {
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        classId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        notificationType: {
+            type: DataTypes.STRING,
+            validate: {
+                isIn: [["newQuestion", "newAnswer", "answerValidation"]]
+            },
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    })
+}
