3b4b84935f32401a3916c305c5e6fd8d70481587
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-07-01 20:51:58 -0300

    added toasts

--- client/src/components/ClassMember.jsx
+++ client/src/components/ClassMember.jsx
@@ -1,5 +1,7 @@
 import image from "../media/image.jpg";
 import React, {useState} from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import axios from "axios";
 
 export const ClassMember =({student, isAdmin, classId}) =>{
@@ -16,7 +18,6 @@ export const ClassMember =({student, isAdmin, classId}) =>{
             permissions: permissions === "teacher" ? "admin": permissions,
             isTeacher: permissions === "teacher" ? 1 : 0
         }).then(res => {
-            alert("Changes have been saved")
             console.log(res)
         })
             .catch(err => console.log(err))
@@ -84,15 +85,22 @@ export const ClassMember =({student, isAdmin, classId}) =>{
             <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                 {/*TODO: create the button to report them (but that's further)*/}
                 {isAdmin ? <>
-                    <button onClick={()=>handleUserKick(student)} type="button"
+                    <button onClick={()=>{
+                        handleUserKick(student)
+                        toast.success("Student has been kicked")
+                    }} type="button"
                             className="focus:outline-none text-white bg-red-700 hover:bg-green-800 focus:ring-4
                                                 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600
                                                 dark:hover:bg-red-700 dark:focus:ring-red-800"><a>Kick student from class</a></button>
                     <button type="button"
-                            onClick={submitChanges}
+                            onClick={() => {
+                                submitChanges()
+                                toast.success("Changes have been submitted")
+                            }}
                             className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                         Confirm Changes
                     </button>
+                    <ToastContainer />
                 </>: null}
             </td>
         </tr>
