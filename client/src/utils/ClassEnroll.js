a02d3cb0c21b26087d54be6dc2ceef2fa80a3274
Tomás <troldanborjas@mail.austral.edu.ar>
2024-07-04 14:18:17 -0300

    Fixed bug where a null json caused ClassEnroll to throw an uncaught exception

--- client/src/utils/ClassEnroll.js
+++ client/src/utils/ClassEnroll.js
@@ -14,6 +14,7 @@ export async function ClassEnroll()   {
         const response = await fetch(endpoint)
         if(!response) return false;
         let responseValue = await response.json()
+        if (!responseValue) return false;
         // console.log(responseValue)
         return responseValue.length !== 0;
     }
