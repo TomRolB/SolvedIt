8a46c81e9dc17e4b01b2a5f6ad9c9a8643cb6ca1
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-07-05 16:14:14 -0300

    notification paging

--- client/src/pages/Notifications.jsx
+++ client/src/pages/Notifications.jsx
@@ -6,6 +6,7 @@ import React, {useEffect, useState} from "react";
 export const Notifications = () => {
     const navigate = useNavigate()
     let [notifications, setNotifications] = useState([])
+    let [page, setPage] = useState(0)
 
     const goToNotificationSettings = () => {
         navigate("/notifications/settings")
@@ -79,11 +80,20 @@ export const Notifications = () => {
                             </tr>
                             </thead>
                             <tbody>
-                            {notifications.map(notification => createEntry(notification))}
+                            {notifications.slice(10 * page, 10 *(page + 1)).map(notification => createEntry(notification))}
                             </tbody>
                         </table>
                     </div>
-
+                </div>
+                <div className="flex justify-center items-center mt-2">
+                    <button type="button" onClick={() => page > 0 ? setPage(page - 1) : setPage(0)}
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                        <i className="fa-solid fa-arrow-left"></i> Previous
+                    </button>
+                    <button type="button" onClick={() => notifications.length / 10 > page + 1 ? setPage(page + 1) : setPage(page)}
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                        Next <i className="fa-solid fa-arrow-right"></i>
+                    </button>
                 </div>
             </div>
         </>
