facfcc44404720c497832eab0ed93ac3cdc44749
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-07-04 23:16:35 -0300

    added ToastContainer to app

--- client/src/components/ClassMember.jsx
+++ client/src/components/ClassMember.jsx
@@ -1,6 +1,6 @@
 import image from "../media/image.jpg";
 import React, {useState} from "react";
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from "axios";
 
@@ -100,7 +100,6 @@ export const ClassMember =({student, isAdmin, classId}) =>{
                             className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                         Confirm Changes
                     </button>
-                    <ToastContainer />
                 </>: null}
             </td>
         </tr>
