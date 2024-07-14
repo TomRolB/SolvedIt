6565cfc75c966de069bed3dffe782c74cd01da27
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-07-13 23:55:57 -0300

    invite checkboxes

--- client/src/pages/class/Invites.jsx
+++ client/src/pages/class/Invites.jsx
@@ -176,8 +176,13 @@ export function Invites() {
                 <Subtitle text={"Create new code"}></Subtitle>
                 <form onSubmit={handleCodeCreation}>
                     {errorMessage ? <h1 color={"red"}>{errorMessage}</h1> : null}
-                    <label>one-time</label>
-                    <input type="checkbox" onChange={handleCheckboxChange} checked={isOneTime}/>
+                    {/*<label>one-time</label>*/}
+                    {/*<input type="checkbox" onChange={handleCheckboxChange} checked={isOneTime}/>*/}
+                    <label className="inline-flex items-center cursor-pointer pt-1 w-max">
+                        <input type="checkbox" checked={isOneTime} className="sr-only peer" id="activated" onClick={handleCheckboxChange}/>
+                        <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                        <span className="ms-3 text-sm font-medium text-gray-900 ">One-time</span>
+                    </label>
                     {input}<br/>
                     <input type="submit" value="Generate"
                            className="h-10 w-40 bg-blue-700 text-white text-xl mt-2 md-2 rounded"/>
@@ -185,9 +190,11 @@ export function Invites() {
                 <div>
                 <Subtitle text={"Link"}></Subtitle>
                         <Copy text={`http://localhost:3000/enroll-to/${id}`} style={{ width: 'auto', height: 'auto' }}></Copy>
-                        <label style={{ marginRight: '1em' }}>Is Active</label>
-                        <input type="checkbox" onChange={handleActiveChange} checked={linkIsActive}/>
-
+                        <label className="inline-flex items-center cursor-pointer pt-1 w-max">
+                            <input type="checkbox" checked={linkIsActive} className="sr-only peer" id="activated" onClick={handleActiveChange}/>
+                            <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                            <span className="ms-3 text-sm font-medium text-gray-900 ">Is Active</span>
+                        </label>
                 </div>
             </div>
         </div>
